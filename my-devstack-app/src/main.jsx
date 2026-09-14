28362a7b42786f090a8866ead907c71b21c2d1c7
shaswatimallik89-ux <shaswatimallik89@gmail.com>
2026-09-14 14:01:15 +0600

    first main commit

--- my-devstack-app/src/main.jsx
+++ my-devstack-app/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ToastContainer } from "react-toastify";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import App from "./App";
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+
+    <ToastContainer
+      position="top-right"
+      autoClose={2500}
+      theme="light"
+    />
+  </React.StrictMode>
+);
